37968db35d9c8d67c0e63651375c0d02c72d9f73
DEJEU <jd0939567@otc.edu>
2022-04-28 09:08:20 -0500

    initial push

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavBar></NavBar>
+    <GridCard></GridCard>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
+import GridCard from './components/GridCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavBar,
+    GridCard
   }
 }
 </script>
@@ -23,6 +25,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
